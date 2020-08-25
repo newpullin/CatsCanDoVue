0800c7c9ba70839d61225ffbdb57d085c004ad49
Yongsoo Na <newpullin@naver.com>
2020-08-25 16:17:07 +0900

    child component emit on props

---
+++ practice4/main.js
@@ -0,0 +1,52 @@
+var myComponent = {
+    template: '<p>MyComponent</p>'
+}
+Vue.component('my-component2', {
+    template: '<p>{{ message }}</p>',
+    data: function() {
+        return {
+            message: 'Hello component'
+        }
+    },
+    methods: {
+        
+    }
+})
+Vue.component('comp-child', {
+    template: '<div><p>자식{{ val }}</p><button @click="handleClick">이벤트호출</button></div>',
+    props: ['val'],
+    methods: {
+        handleClick: function() {
+            console.log('자식 이벤트 발생')
+            this.$emit('childs-event')
+        }
+    }
+})
+
+new Vue({
+    el: '#app',
+    data: {
+      msg1 : 'A',
+      msg2 : 'B',  
+    },
+    components: {
+        'my-component':myComponent
+    },
+    methods: {
+        change_child: function() {
+            console.log("?")
+            if (this.msg1 === 'A') {
+                this.msg1 = 'B'
+                this.msg2 = 'A'
+            }
+            else {
+                this.msg1 = 'A'
+                this.msg2 = 'B'
+            }
+        },
+        parentsMethod: function() {
+            console.log("이벤트를 받았습니다.")
+        }
+    }
+})
+
