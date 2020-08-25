83c0074aebf7e2ab85a118bbc674706b43a56ea2
Yongsoo Na <newpullin@naver.com>
2020-08-25 11:05:56 +0900

    set class and style with object with v-bind

--- practice/main.js
+++ practice/main.js
@@ -2,7 +2,15 @@ var app = new Vue({
     el: '#app',
     data: {
         //객체 데이터
-        count :0
+        count :0,
+        classObject : {
+            child:true,
+            'is-active': false,
+        },
+        styleObject : {
+            color: 'red',
+            backgroundColor: "lightgray",
+        }
     },
     methods: {
         increment: function() {
