f2a54c569a1bd3e542f68e36f57c1f4c8f7222f3
Yongsoo Na <newpullin@naver.com>
2020-08-25 16:36:35 +0900

    I want to use Vue file

--- practice4/main.js
+++ practice4/main.js
@@ -12,14 +12,34 @@ Vue.component('my-component2', {
         
     }
 })
+var bus = new Vue({
+    data: {
+        count: 0
+    }
+})
 Vue.component('comp-child', {
-    template: '<div><p>자식{{ val }}</p><button @click="handleClick">이벤트호출</button></div>',
+    template: '<div><p>자식{{ val }}</p><button @click="handleClick">이벤트호출</button><button @click="busTest">버스테스트</button></div>',
     props: ['val'],
     methods: {
         handleClick: function() {
             console.log('자식 이벤트 발생')
             this.$emit('childs-event')
+        },
+        busTest: function() {
+            console.log('버스 이벤트 발생')
+            bus.$emit('bus-event')
         }
+    }
+})
+Vue.component('comp-b', {
+    template: '<p> {{ bus.count }} </p>',
+    computed: {
+        bus: function() { return bus.$data}
+    },
+    created: function() {
+        bus.$on('bus-event', function() {
+            this.count++
+        })
     }
 })
 
---
+++ vue/test.vue
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue.js App</title>
+    <style>
+    </style>
+</head>
+<body>
+    <div id="app">
+        <!-- #app 내부에 템플릿을 출력할 것입니다.-->
+        <h1>{{ message }}</h1>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10"></script>
+    <script>
+    new Vue({
+        el: '#app',
+        data:{
+            message: 'hello world!',
+        }
+    })
+    </script>
+</body>
+</html>
