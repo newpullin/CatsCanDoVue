a2f573e13aae4f2758c888494df5f039838031a1
Yongsoo Na <newpullin@naver.com>
2020-08-25 11:31:13 +0900

    v-for and v-bind and add list

--- practice/main.js
+++ practice/main.js
@@ -13,10 +13,27 @@ var app = new Vue({
         },
         radius: 50,
         x_pos : 100,
+        name: '키메라',
+        list: [
+            {id: 1, name: '슬라임', hp: 100},
+            {id: 2, name: '슬라임2', hp: 200},
+            {id: 3, name: '슬라임3', hp: 300}
+        ]
     },
     methods: {
         increment: function() {
             this.count += 1
+        },
+        doAdd: function() {
+            var max = this.list.reduce(function(a, b) {
+                return a > b.id ? a : b.id
+            }, 0)
+
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500
+            })
         }
     }
 })
