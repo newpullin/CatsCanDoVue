030b83e2aad442fa40781774bd982787719ce528
Yongsoo Na <newpullin@naver.com>
2020-08-25 10:51:53 +0900

    increment count with v-on:click

--- practice/main.js
+++ practice/main.js
@@ -2,10 +2,11 @@ var app = new Vue({
     el: '#app',
     data: {
         //객체 데이터
-        message: {
-            value: 'Hello Vue.js',
-        },
-        list: ['사과', '바나나', '두리안'],
-        num : 1,
+        count :0
+    },
+    methods: {
+        increment: function() {
+            this.count += 1
+        }
     }
 })
